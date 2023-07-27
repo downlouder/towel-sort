39db1868c929207e5d737f7e137a990a5b268b6a
Nazarenko Yurii <wallpapeforpc@gmail.com>
2023-07-27 18:37:35 +0300

    feat: finished task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (matrix = []) {
+  return matrix.reduce((acc, value, index) => {
+    value.sort((a, b) => !(index % 2) ? a - b : b - a).map(element => acc.push(element));
+    return acc;
+  }, [])
 }
